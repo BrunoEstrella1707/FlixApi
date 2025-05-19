2e20bdb773ef160e450cdc1242cdf234943b53cf
brunoestrella <brunogamer2002@gmail.com>
2025-05-19 12:19:08 -0300

    feat: added movie titles in reviews

--- reviews/serializers.py
+++ reviews/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from reviews.models import Review
+from movies.models import Movie
 
 
 class ReviewSerializer(serializers.ModelSerializer):
@@ -7,3 +8,11 @@ class ReviewSerializer(serializers.ModelSerializer):
     class Meta:
         model = Review
         fields = '__all__'
+
+
+class ReviewListDetailSerializer(serializers.ModelSerializer):
+    movie_title = serializers.CharField(source='movie.title', read_only=True)
+
+    class Meta:
+        model = Review
+        fields = ['id', 'stars', 'comment', 'movie', 'movie_title']
--- reviews/views.py
+++ reviews/views.py
@@ -1,17 +1,28 @@
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 from reviews.models import Review
-from reviews.serializers import ReviewSerializer
+from reviews.serializers import ReviewSerializer, ReviewListDetailSerializer
 from app.permissions import GlobalDefaultPermission
 
 
 class ReviewCreateListView(generics.ListCreateAPIView):
     permission_classes = (IsAuthenticated, GlobalDefaultPermission,)
     queryset = Review.objects.all()
-    serializer_class = ReviewSerializer
+
+    def get_serializer_class(self):
+        if self.request.method == 'GET':
+            return ReviewListDetailSerializer
+        else:
+            return ReviewSerializer
+
 
 
 class ReviewRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = (IsAuthenticated, GlobalDefaultPermission,)
     queryset = Review.objects.all()
-    serializer_class = ReviewSerializer
+    
+    def get_serializer_class(self):
+        if self.request.method == 'GET':
+            return ReviewListDetailSerializer
+        else:
+            return ReviewSerializer
